9c3d78c3b5258ff4572f51ea8f0be3477cdae745
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-02-23 17:31:11 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.main}>
+      <div className={style.box}>
+        <h1>Rick and Morty</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
