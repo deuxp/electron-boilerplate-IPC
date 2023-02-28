2b4ceab5c6462697251db585cbbf3bf3d68dbb98
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-02-27 23:16:59 -0500

    migrate logic to IPC communication

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,35 +1,18 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 
 function useData() {
   const [data, setData] = useState({});
 
-  function getRandomIndex() {
-    let num;
-    num = Math.random() * 825;
-    num = Math.floor(num);
-    return num > 0 ? num : 1;
-  }
-  const getCharacter = useCallback(() => {
-    let index;
-    index = getRandomIndex();
-    fetch(`https://rickandmortyapi.com/api/character/${index}`)
-      .then(res => {
-        let data = res.json();
-        return data;
-      })
-      .then(data => {
-        console.log(data);
-        setData(data);
-        return data;
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+  const getCharacter = () => {
+    window.bridge.fetchCharacter(response => {
+      console.log(response.id);
+      setData(response);
+    });
+  };
 
   useEffect(() => {
     getCharacter();
-  }, [getCharacter]);
+  }, []);
 
   return { data, getCharacter };
 }
