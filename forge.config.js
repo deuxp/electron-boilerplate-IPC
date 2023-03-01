4841d3fced2c1d174eca60c477de3f472c48ac11
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-01 14:57:02 -0500

    config rules and dmg

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,9 @@
+const APP_NAME = "r-and-m-character-roller";
 module.exports = {
   packagerConfig: {
-    icon: "public/favicon.ico",
-    name: "Rick and Morty Character Roller",
+    icon: "src/icons/png/32x32.png",
+    name: APP_NAME,
+    asar: true,
   },
   rebuildConfig: {},
   makers: [
@@ -9,6 +11,10 @@ module.exports = {
       name: "@electron-forge/maker-squirrel",
       config: {
         icon: "src/icons/win.icon.ico",
+        name: APP_NAME,
+        author: "Gottfried K",
+        exe: `${APP_NAME}.exe`,
+        description: "Roll a random character from the rick and morty universe",
       },
     },
     {
@@ -16,18 +22,31 @@ module.exports = {
       platforms: ["darwin"],
       config: {
         icon: "src/icons/mac/icon.icns",
+        name: APP_NAME,
       },
     },
     {
       name: "@electron-forge/maker-deb",
+      platforms: ["linux"],
       config: {
         icon: "src/icons/png/32x32.png",
+        name: APP_NAME,
+        description: "Roll a random character from the rick and morty universe",
       },
     },
     {
       name: "@electron-forge/maker-rpm",
       config: {
         icon: "src/icons/png/32x32.png",
+        name: APP_NAME,
+      },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        background: "./src/icons/png/512x512.png",
+        format: "ULFO",
+        name: APP_NAME,
       },
     },
   ],
