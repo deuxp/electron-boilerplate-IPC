592140a62b51df590e0e7fe98ea260f3efb52c24
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-22 17:41:00 -0400

    resetPassword & postNewPassword

--- public/preload.js
+++ public/preload.js
@@ -3,16 +3,16 @@ const { contextBridge, ipcRenderer } = require("electron");
 const ipcBridge = {
   fetchCharacter: callback => {
     ipcRenderer.invoke("getCharacter");
-    const listener = ipcRenderer.on("renderProcListener", (event, response) => {
-      const data = JSON.parse(response);
+    const listener = ipcRenderer.on("renderProcListener", (event, res) => {
+      const data = JSON.parse(res);
       callback(data);
       listener.removeAllListeners("renderProcListener");
     });
   },
   getHoney: callback => {
     ipcRenderer.invoke("getHoney");
-    const listener = ipcRenderer.on("renderHoney", (event, response) => {
-      const data = JSON.parse(response);
+    const listener = ipcRenderer.on("renderHoney", (event, res) => {
+      const data = JSON.parse(res);
       console.log(data);
       callback(data);
       listener.removeAllListeners("renderHoney");
@@ -20,8 +20,8 @@ const ipcBridge = {
   },
   refresh: callback => {
     ipcRenderer.invoke("refresh");
-    const listener = ipcRenderer.on("renderRefresh", (event, response) => {
-      const data = JSON.parse(response);
+    const listener = ipcRenderer.on("renderRefresh", (event, res) => {
+      const data = JSON.parse(res);
       console.log(data);
       callback(data);
       listener.removeAllListeners("renderRefresh");
@@ -30,18 +30,37 @@ const ipcBridge = {
   login: (credentials, callback) => {
     const creds = JSON.stringify(credentials);
     ipcRenderer.invoke("login", creds);
-    const listener = ipcRenderer.on("renderLogin", (event, response) => {
-      const data = JSON.parse(response);
+    const listener = ipcRenderer.on("renderLogin", (event, res) => {
+      const data = JSON.parse(res);
       console.log(data);
       callback(data);
       listener.removeAllListeners("renderLogin");
     });
   },
+  resetPassword: (email, callback) => {
+    ipcRenderer.invoke("resetPassword", email);
+    const listener = ipcRenderer.on("renderResetPassword", (e, res) => {
+      const data = JSON.parse(res);
+      console.log(data);
+      callback(data);
+      listener.removeAllListeners("renderResetPassword");
+    });
+  },
+  postNewPassword: (credentials, callback) => {
+    const creds = JSON.stringify(credentials);
+    ipcRenderer.invoke("postNewPassword", creds);
+    const listener = ipcRenderer.on("renderNewPassword", (event, res) => {
+      const data = JSON.parse(res);
+      console.log(data);
+      callback(data);
+      listener.removeAllListeners("renderNewPassword");
+    });
+  },
   register: (credentials, callback) => {
     const creds = JSON.stringify(credentials);
     ipcRenderer.invoke("register", creds);
-    const listener = ipcRenderer.on("renderRegister", (event, response) => {
-      const data = JSON.parse(response);
+    const listener = ipcRenderer.on("renderRegister", (event, res) => {
+      const data = JSON.parse(res);
       console.log(data);
       callback(data);
       listener.removeAllListeners("renderRegister");
