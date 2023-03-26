6c746d166a0e710c8c6b8c196b29b78694cdb2e5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-26 14:31:05 -0400

    set actions

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Login from "./components/Login/Login";
 function App() {
   const { data, getCharacter, isLoggedIn, setIsLoggedIn } = useData();
 
-  // promise me, pls
+  // make promise
   const refreshToken = () => {
     window.bridge.refresh(res => {
       if (res.refresh) {
